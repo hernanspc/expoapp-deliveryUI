e8e49998d7473b22e0e04f50a9903bd94b23971e
Hernan Pormachi Quichca <hernan.pormachi.quichca@everis.nttdata.com>
2022-10-19 09:25:05 -0500

    init changes

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
+import { useFonts } from 'expo-font';
+
+import {
+  MainLayout,
+} from "./screens";
+
+const Stack = createStackNavigator();
+
+const App = () => {
+
+  const [loaded] = useFonts({
+    "Poppins-Black": require('./assets/fonts/Poppins-Black.ttf'),
+    "Poppins-Bold": require('./assets/fonts/Poppins-Bold.ttf'),
+    "Poppins-SemiBold": require('./assets/fonts/Poppins-SemiBold.ttf'),
+    "Poppins-Regular": require('./assets/fonts/Poppins-Regular.ttf'),
+  })
+
+  if (!loaded) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false
+        }}
+        initialRouteName={'Home'}
+      >
+        <Stack.Screen
+          name="Home"
+          component={MainLayout}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
